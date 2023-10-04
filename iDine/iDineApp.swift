621058ea370536e3c09a09f98064622cf2c16555
Bryan Mansell <bryan@bryanmansell.co.uk>
2023-10-04 18:35:05 +0100

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Bryan Mansell on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
