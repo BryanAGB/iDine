fdd52c634de8f45d2d4f1d5ffd3cb8531040d65e
Bryan Mansell <bryan@bryanmansell.co.uk>
2023-10-05 18:00:30 +0100

    add menu and items

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,41 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Bryan Mansell on 04/10/2023.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    
+    let item: MenuItem
+    let colors : [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
+    
+    var body: some View {
+        HStack{
+            Image(item.thumbnailImage)
+                .clipShape(.circle)
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                Text("$\(item.price)")
+            }
+            Spacer()
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundStyle(.white)
+            }
+        }
+    }
+}
+
+#Preview {
+    ItemRow(item: MenuItem.example)
+}
